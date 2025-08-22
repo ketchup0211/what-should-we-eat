40451e4ca3c93955599833d3d502ce1ce82bee41
ketchup0211 <dlcka1111@naver.com>
2025-08-23 00:42:17 +0900

    [ADD] Styled component implementation

--- src/components/SlotMachine.jsx
+++ src/components/SlotMachine.jsx
@@ -1,11 +1,39 @@
 import useRandomFood from "../hooks/useRandomFood";
+import styled from "styled-components";
 
 export default function SlotMachine({ foods }) {
   const { randomFood, pickRandomFood } = useRandomFood(foods);
   return (
-    <div>
-      <h1>{randomFood}</h1>
-      <button onClick={pickRandomFood}>랜덤 룰렛</button>
-    </div>
+    <Container>
+      <p>오늘의 메뉴는</p>
+      <Result>{randomFood}</Result>
+      <Button onClick={pickRandomFood}>랜덤 룰렛</Button>
+    </Container>
   );
 }
+
+//styled-components
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+`;
+const Result = styled.h1`
+  padding: 10px;
+  font-size: 42px;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+`;
+const Button = styled.button`
+  background-color: tomato;
+  color: white;
+  border: none;
+  border-radius: 25px;
+  cursor: pointer;
+  padding: 15px;
+  margin: 10px;
+
+  &:hover {
+    filter: brightness(0.95);
+  }
+`;
