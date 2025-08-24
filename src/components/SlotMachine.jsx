434ed83d3cde541b1c97333d245a448079fb6c24
ketchup0211 <dlcka1111@naver.com>
2025-08-24 20:27:36 +0900

    [ADD] buttons with clicked state

--- src/components/SlotMachine.jsx
+++ src/components/SlotMachine.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import useRandomFood from "../hooks/useRandomFood";
 import styled from "styled-components";
 
 export default function SlotMachine({ foods }) {
   const { randomFood, pickRandomFood } = useRandomFood(foods);
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    pickRandomFood();
+    setClicked(true);
+  };
+
   return (
     <Container>
       <p>오늘의 메뉴는</p>
       <Result>{randomFood}</Result>
-      <Button onClick={pickRandomFood}>랜덤 룰렛</Button>
+      {clicked ? (
+        <ButtonContainer>
+          <Button>싫어!</Button>
+          <Button>맛있겠다!</Button>
+        </ButtonContainer>
+      ) : (
+        <Button onClick={handleClick}>랜덤 룰렛</Button>
+      )}
     </Container>
   );
 }
@@ -19,6 +34,13 @@ const Container = styled.div`
   align-items: center;
   margin-top: 50px;
 `;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
 const Result = styled.h1`
   padding: 10px;
   font-size: 42px;
