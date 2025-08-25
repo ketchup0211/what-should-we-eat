49406868b46c5f8956d4271bdae26d203a1b407d
ketchup0211 <dlcka1111@naver.com>
2025-08-26 01:28:05 +0900

    [ADD] Menu Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import MenuBar from "./components/MenuBar";
 
 function App() {
   return (
     <Router>
+      <MenuBar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
---
+++ src/components/MenuBar.jsx
@@ -0,0 +1,7 @@
+export default function MenuBar() {
+  return (
+    <div>
+      <h1>MENU</h1>
+    </div>
+  );
+}
