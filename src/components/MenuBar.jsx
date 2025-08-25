e6665f40035f7aa32ba1543106e00b8f550fbf5d
ketchup0211 <dlcka1111@naver.com>
2025-08-26 02:17:06 +0900

    [ADD] MenuBar CSS(ver.1) complete

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -4,18 +4,21 @@ import { Link } from "react-router-dom";
 export default function MenuBar() {
   return (
     <Container>
-      <h1>
-        <Link to="/">Home</Link>
-      </h1>
-      <h1>
-        <Link to="/food-list">FoodList</Link>
-      </h1>
-      <h1>
-        <Link to="/custom-rulet">CustomRulet</Link>
-      </h1>
-      <h1>
-        <Link to="/about">About</Link>
-      </h1>
+      <MenuLink to="/" title="홈으로">
+        밥상공론
+      </MenuLink>
+
+      <MenuLink to="/food-list" title="밥상 후보 선정하러 가기">
+        메뉴판 수정
+      </MenuLink>
+
+      <MenuLink to="/custom-rulet" title="원하는 메뉴로 밥상 차리기">
+        내맘대로 밥상
+      </MenuLink>
+
+      <MenuLink to="/about" title="메뉴 선정에 진심인 개발자에 대해">
+        개발자 나와라
+      </MenuLink>
     </Container>
   );
 }
@@ -28,4 +31,19 @@ const Container = styled.div`
   justify-content: space-around;
   align-items: center;
   padding: 20px;
+`;
+
+const MenuLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+  font-size: 1.1rem;
+  font-weight: 400;
+  &:hover {
+    filter: opacity(0.7);
+    transition: 0.2s ease;
+  }
+  &:active {
+    color: white;
+    transform: scale(0.8);
+  }
 `;
