50236f9e1760a660df9bdefcfb8fdf62c22a485f
ketchup0211 <dlcka1111@naver.com>
2025-08-27 03:40:15 +0900

    [MOD] FoodList CSS frame

--- src/components/FoodList/Uploaded.jsx
+++ src/components/FoodList/Uploaded.jsx
@@ -1,12 +1,55 @@
+import styled from "styled-components";
 import getCurrentFoodList from "../../utils/getCurrentFoodList";
 
 export default function Uploaded() {
+  const foodList = getCurrentFoodList();
   return (
-    <div>
-      <p>현재 음식 리스트 목록</p>
-      {getCurrentFoodList().map((food, id) => {
-        return <p key={id}>{food}</p>;
-      })}
-    </div>
+    <Container>
+      <Title>현재 음식 목록 ({foodList.length})</Title>
+      <FoodContainer>
+        {foodList.map((food, id) => {
+          return <Food key={id}>{food}</Food>;
+        })}
+      </FoodContainer>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+  overflow: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const Title = styled.div`
+  font-size: 2rem;
+  padding: 30px 10px;
+  position: sticky;
+  top: 0px;
+  background-color: gray;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+`;
+const FoodContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+
+  font-size: 1.4rem;
+  gap: 3px;
+`;
+
+const Food = styled.p`
+  width: 100%;
+  background-color: white;
+  padding: 5px 30px;
+  &:hover {
+    cursor: pointer;
+    filter: opacity(0.8);
+  }
+`;
